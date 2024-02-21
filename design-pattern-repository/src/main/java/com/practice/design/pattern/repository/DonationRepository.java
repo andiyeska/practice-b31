54a641d852eb3f7417920dfa26bc28d35806459b
Andi Yeskafauzan <andi.yeskafauzan@gdn-commerce.com>
2024-02-21 23:51:39 +0700

    add donation

---
+++ design-pattern-repository/src/main/java/com/practice/design/pattern/repository/DonationRepository.java
@@ -0,0 +1,7 @@
+package com.practice.design.pattern.repository;
+
+import com.practice.design.pattern.entity.Donation;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface DonationRepository extends ReactiveMongoRepository<Donation, String> {
+}
